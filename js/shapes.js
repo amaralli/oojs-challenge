9db04bc60b1aa9b5e0c85f94b05f9aa50b647ed2
Allison Amaral <amaralli@uw.edu>
2014-12-05 01:07:11 -0800

    Updated all the shapes!

--- js/shapes.js
+++ js/shapes.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /*
     shapes.js
     This is where your code goes
@@ -13,3 +15,50 @@
     or registerClassicalShape() to register your new shapes with the application. See the
     app.js file for info on these functions.
  */
+
+function createRectangle(left, top, width, height, stylesMap) {
+    var rectangle = createShape(left, top, width, height, stylesMap);
+
+    rectangle.renderShape = function(canvasCtx) {
+        canvasCtx.fillRect(this.left, this.top, this.width, this.height);
+    }
+
+    return rectangle;
+}
+
+registerPrototypalShape('Rectangle', createRectangle);
+
+function createCircle(left, top, width, height, stylesMap) {
+    var circle = createShape(left, top, width, height, stylesMap);
+
+    circle.renderShape = function(canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(this.left, this.top, this.width / 2, 0, 2 * Math.PI);
+        canvasCtx.fill();
+        canvasCtx.stroke();
+    }
+
+    return circle;
+}
+
+registerPrototypalShape('Circle', createCircle);
+
+function createText(left, top, right, width, height, stylesMap) {
+    var text = createShape(left, top, width, height, stylesMap);
+
+    text.renderShape = function (canvasCtx) {
+        canvasCtx.font="20px Verdana";
+        var canvas = document.getElementById("shapes-canvas");
+        var gradient = canvasCtx.createLinearGradient(0, 0, canvas.width, canvas.height);
+        gradient.addColorStop("0", "blue");
+        gradient.addColorStop("0.33", "green");
+        gradient.addColorStop("0.67", "yellow");
+        gradient.addColorStop("1.0", "orange");
+        canvasCtx.fillStyle = gradient;
+        canvasCtx.fillText("Hello!", this.left, this.top);
+    }
+
+    return text;
+}
+
+registerPrototypalShape('Text', createText);
